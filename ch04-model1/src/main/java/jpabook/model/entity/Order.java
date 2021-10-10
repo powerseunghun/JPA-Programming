b90dbdb167738e63c7f1144a25acbbe77aaf7fec
anseunghun <size411@naver.com>
2021-10-10 23:15:47 +0900

    Practical Example Add Entity

--- ch04-model1/src/main/java/jpabook/model/entity/Order.java
+++ ch04-model1/src/main/java/jpabook/model/entity/Order.java
@@ -1,59 +1,55 @@
 package jpabook.model.entity;
 
-import javax.persistence.*;
 import java.util.Date;
 
-/**
- * Created by holyeye on 2014. 3. 11..
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 @Entity
-@Table(name = "ORDERS")
+@Table(name="ORDERS")
 public class Order {
-
-    @Id
-    @GeneratedValue
-    @Column(name = "ORDER_ID")
-    private Long id;
-
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date orderDate;     //주문시간
-
-    @Enumerated(EnumType.STRING)
-    private OrderStatus status;//주문상태
-
-    //Getter, Setter
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public Date getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
+	@Id @GeneratedValue
+	@Column(name="ORDER_ID")
+	private Long id;
+	
+	@Column(name="MEMBER_ID")
+	private Long memberId;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date orderDate;
+	
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public OrderStatus getStatus() {
+		return status;
+	}
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
 }
